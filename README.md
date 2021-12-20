a7a11bb4b078633866a942d24bd35b105e4099a0
jesserwight <jessewright01@gmail.com>
2021-12-20 15:20:22 -0500

    move config out into .env file

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# EU-US-FORMS-MIGRATION
-1. This is a rust project with the source file at `src/main.rs`
-1. This assumes a UNIX-like shell.
-1. Download rust `curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh` or see https://www.rust-lang.org/tools/install
-1. A valid `cert.pem` file must be present in the root directory to connect to MongoDB. It must be named `cert.pem`.
-1. Ensure appropriate environment variables are in the `FROM` and `TO` structures at the top of `src/main.rs` file. Both the S3 and MongoDB migrations depend on these values.
-1. To run the MongoDB migration, run `cargo r > output.txt` in the root directory. Output of the migration will be in `output.txt`
-1. To run the S3 migration: Have the AWS CLI installed and configured, uncomment the first *two* lines within the `main` function, and run `cargo r > copy_s3.sh && chmod+x copy_s3.sh`. This writes an executable script to the root folder called `copy_s3.sh`. Run this with `./copy_s3.sh`.
+- Install the Rust programming language toolchain: `curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh`
+- A valid `cert.pem` file must be present in the current for MongoDB connection.
+- `aws` must be in path, configured with relevant permissions.
+- Run `cargo r` to perform the migration (both mongo and s3 migrations are done with this command).
+- A log of changes made during the migration are streamed to STDOUT
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use dotenv::dotenv;
+use envy;
 use futures::stream::TryStreamExt;
 use mongodb::{
     bson::Document,
@@ -6,87 +8,84 @@ use mongodb::{
     },
     Client,
 };
-
-const FROM: MongoConfig = MongoConfig {
-    s3_bucket_name: "dev1-good-tenant-private",
-    region: "us-west-2",
-    host: "dev1-platform.cluster-cti10lnrh4rb.us-west-2.docdb.amazonaws.com",
-    username: "root",
-    password: "cWgSdoCUjSCuylBa",
-    db: "dev1-good-form-template",
-    collection: "trial-416c6768-10af-4974-9004-6f5164ff8416",
-    domain: "s37dev",
-    instance: "dev1-platform",
-    tenant: "good",
-    trial: "416c6768-10af-4974-9004-6f5164ff8416",
-};
-const TO: MongoConfig = MongoConfig {
-    s3_bucket_name: "dev1-good-tenant-private",
-    region: "us-west-2",
-    host: "dev2-platform.cluster-cti10lnrh4rb.us-west-2.docdb.amazonaws.com",
-    username: "root",
-    password: "nKuvxmOUuP9ti528",
-    db: "dev2-acme-form-template",
-    collection: "trial-fcf331c5-aa89-4b9a-af94-513fefc00aa4",
-    instance: "dev2-platform",
-    domain: "s37dev",
-    tenant: "acme",
-    trial: "fcf331c5-aa89-4b9a-af94-513fefc00aa4",
-};
+use serde::{Deserialize, Serialize};
+use tokio::process::Command;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // println!("{}", generate_aws_script());
-    // return Ok(()); // this is an early return to the migration does not run
+    dotenv().ok();
+    let from = envy::prefixed("FROM_").from_env::<MongoConfig>()?;
+    let to = envy::prefixed("TO_").from_env::<MongoConfig>()?;
 
-    // Print the state before migration
     println!("FROM");
-    print_collection(&FROM).await?;
+    print_collection(&from).await?;
     println!("TO");
-    print_collection(&TO).await?;
-
-    // Query the FROM db and do the transformation on each document
-    let from_url = generate_url(&FROM);
-    let to_url = generate_url(&TO);
-    let transformed_from_docs: Vec<_> = build_connection(&FROM)
+    print_collection(&to).await?;
+    let modified_from_docs: Vec<_> = build_connection(&from)
         .await?
-        .database(FROM.db)
-        .collection::<Document>(FROM.collection)
+        .database(&from.db)
+        .collection::<Document>(&from.collection)
         .find(None, None)
         .await?
         .try_collect::<Vec<_>>()
         .await?
         .iter_mut()
         .map(|doc| {
-            println!("----------------");
-            println!("{}", doc.get("id").unwrap());
-            println!("{}", doc.get("title").unwrap());
-            let mut v: Vec<u8> = Vec::new();
-            doc.to_writer(&mut v).unwrap();
-            // Swap the URLS **first**
-            insert_chars_eq(&mut v, &from_url, &to_url);
-            // Swap the IDs
-            let from_trial = &FROM.trial.chars().map(|c| c as u8).collect::<Vec<_>>();
-            let to_trial = &TO.trial.chars().map(|c| c as u8).collect::<Vec<_>>();
-            insert_chars_eq(&mut v, &from_trial, &to_trial);
-            let modified_doc = Document::from_reader(std::io::Cursor::new(v)).unwrap();
-            modified_doc
+            let new_doc = modify_doc(&from, &to, doc);
+            return new_doc;
         })
         .collect();
 
-    // Write the results of the FROM db + transformation to TO db
-    build_connection(&TO)
+    build_connection(&to)
         .await?
-        .database(TO.db)
-        .collection(TO.collection)
-        .insert_many(transformed_from_docs, None) //   <- insert the forms
-        .await
-        .expect("insertion failure");
+        .database(&to.db)
+        .collection(&to.collection)
+        .insert_many(modified_from_docs, None)
+        .await?;
 
-    // Print the state after migration
     println!("TO AFTER");
-    print_collection(&TO).await?;
+    print_collection(&to).await?;
+
+    let s3_cmd = Command::new("aws")
+        .args([
+            "s3",
+            "cp",
+            &format!("s3://{}/form-template/{}/", &from.s3_bucket, &from.trial),
+            &format!("s3://{}/form-template/{}/", &to.s3_bucket, &to.trial),
+            "--recursive",
+            "--source-region",
+            &from.region,
+            "--region",
+            &to.region,
+        ])
+        .output()
+        .await?;
+    let s3_cmd_output = s3_cmd.stdout.iter().map(|u| *u as char).collect::<String>();
+    println!("S3 command output:\n{}", s3_cmd_output);
+
     Ok(())
+}
+
+fn print_doc(doc: &Document) {
+    println!("{} {}", doc.get("id").unwrap(), doc.get("title").unwrap());
+}
+
+fn modify_doc(from: &MongoConfig, to: &MongoConfig, doc: &mut Document) -> Document {
+    println!("----------------");
+    print_doc(&doc);
+    let mut v: Vec<u8> = Vec::new();
+    doc.to_writer(&mut v).unwrap();
+    // Swap the URLS first
+    let from_url = generate_url(from);
+    let to_url = generate_url(to);
+    insert_chars_eq(&mut v, &from_url, &to_url);
+    // Swap the IDs
+    // doc.get_mut("id").unwrap() = to.trial; // <- better way of doing this by getting a mutable ref?
+    let from_trial = &from.trial.chars().map(|c| c as u8).collect::<Vec<_>>();
+    let to_trial = &to.trial.chars().map(|c| c as u8).collect::<Vec<_>>();
+    insert_chars_eq(&mut v, &from_trial, &to_trial);
+    let modified_doc = Document::from_reader(std::io::Cursor::new(v)).unwrap();
+    modified_doc
 }
 
 // given `a` includes a slice `b`, replace `b` with `c`
@@ -107,51 +106,32 @@ fn insert_chars_eq(a: &mut [u8], b: &[u8], c: &[u8]) {
     }
 }
 
-struct MongoConfig<'a> {
-    s3_bucket_name: &'a str,
-    region: &'a str,
-    host: &'a str,
-    username: &'a str,
-    password: &'a str,
-    db: &'a str,
-    collection: &'a str,
-    instance: &'a str,
-    tenant: &'a str,
-    trial: &'a str,
-    domain: &'a str,
+#[derive(Deserialize, Debug)]
+struct MongoConfig {
+    s3_bucket: String,
+    region: String,
+    host: String,
+    username: String,
+    password: String,
+    db: String,
+    collection: String,
+    instance: String,
+    tenant: String,
+    trial: String,
+    domain: String,
 }
 
-fn generate_s3_uri(cfg: MongoConfig) -> String {
-    format!("s3://{}/form-template/{}/", cfg.s3_bucket_name, cfg.trial)
-}
-
-fn generate_aws_script() -> String {
-    format!(
-        r#"
-#! /bin/bash
-# This file is generated. Maybe don't modify it.
-aws s3 cp {} {} --recursive --source-region {} --region {}
-"#,
-        generate_s3_uri(FROM),
-        generate_s3_uri(TO),
-        FROM.region,
-        TO.region
-    )
-}
-
-async fn print_collection(
-    mongo_config: &MongoConfig<'_>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    for f in build_connection(&mongo_config)
+async fn print_collection(mongo_config: &MongoConfig) -> Result<(), Box<dyn std::error::Error>> {
+    for doc in build_connection(&mongo_config)
         .await?
-        .database(mongo_config.db)
-        .collection(mongo_config.collection)
-        .find(None, None) //  <- get the forms
+        .database(&mongo_config.db)
+        .collection(&mongo_config.collection)
+        .find(None, None)
         .await?
         .try_collect::<Vec<Document>>()
         .await?
     {
-        println!("{} {}", f.get("id").unwrap(), f.get("title").unwrap(),);
+        print_doc(&doc)
     }
 
     Ok(())
@@ -165,17 +145,17 @@ fn generate_url(cfg: &MongoConfig) -> Vec<u8> {
     s.chars().map(|c| c as u8).collect::<Vec<_>>()
 }
 
-async fn build_connection(config: &MongoConfig<'_>) -> Result<Client, Box<dyn std::error::Error>> {
+async fn build_connection(config: &MongoConfig) -> Result<Client, Box<dyn std::error::Error>> {
     let tls = TlsOptions::builder()
         .ca_file_path(Some("cert.pem".into()))
         .build();
     let credential = Credential::builder()
-        .username(Some(config.username.into()))
-        .password(Some(config.password.into()))
+        .username(Some(config.username.clone()))
+        .password(Some(config.password.clone()))
         .build();
     let options = ClientOptions::builder()
         .hosts(vec![ServerAddress::Tcp {
-            host: config.host.into(),
+            host: config.host.clone(),
             port: None,
         }])
         .direct_connection(Some(true))
@@ -188,6 +168,19 @@ async fn build_connection(config: &MongoConfig<'_>) -> Result<Client, Box<dyn st
     let client = Client::with_options(options)?;
     return Ok(client);
 }
+// fn generate_aws_script(from: &MongoConfig, to: &MongoConfig) -> String {
+//     format!(
+//         r#"
+// #! /bin/bash
+// # This file is generated. Maybe don't modify it.
+// aws s3 cp {} {} --recursive --source-region {} --region {}
+// "#,
+//         generate_s3_uri(from),
+//         generate_s3_uri(to),
+//         from.region,
+//         to.region
+//     )
+// }
 
 #[test]
 fn test_insert_chars() {
@@ -239,3 +232,209 @@ fn doc_ser_de() {
 // println!("---------");
 // print_collection(&to).await?;
 // return Ok(());
+
+// // "move only these ids"
+// let ids = [
+//     "1e7a2fec-e692-4454-8c79-ff0b99b705ea",
+//     "f3cfe220-b6e0-4901-9378-ad9f783302db",
+//     "2cf59d34-4c78-430b-83c0-cee0b50a5b74",
+//     "0065850f-1aee-4b42-869b-ef2c3642cf8e",
+//     "d541f46c-7185-4b14-b40c-da2f8be08a4d",
+//     "399a1461-bca1-4e88-982a-2236073560f3",
+//     "8c429c4c-3b6d-411d-8f98-3f0badd9b228",
+// ];
+
+// let db_tmp = build_connection(&from)
+//     .await?
+//     .database(&from.db)
+//     .collection::<Document>(&from.collection);
+// let mut to_move: Vec<Document> = Vec::new();
+
+// for id in ids {
+//     let t = db_tmp
+//         .find_one(mongodb::bson::doc! {"id": id}, None)
+//         .await?
+//         .unwrap();
+//     to_move.push(t);
+// }
+
+// let modified_from_docs: Vec<_> = to_move
+//     .iter_mut()
+//     .map(|doc| {
+//         print_doc(&doc);
+//         let new_doc = modify_doc(&from, &to, doc);
+//         return new_doc;
+//     }).collect();
+
+use mongodb::bson;
+use serde_json::Value;
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FormTemplate {
+    id: String,
+    title: String,
+    trial_id: String,
+    created_by: String,
+    updated_by: String,
+    title_to_search: String,
+    form_oid: String,
+    form_oid_to_search: String,
+    created_on: bson::DateTime,
+    last_updated_on: bson::DateTime,
+    tags: Vec<Tag>,
+    status: String,
+    form_type: String,
+    version: i64,
+    template: Template,
+    settings: Vec<Setting>,
+    pdf_templates: PdfTemplates,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Template {
+    resource_type: String,
+    id: String,
+    extension: Vec<Extension>,
+    url: String,
+    title: String,
+    status: String,
+    date: bson::DateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ValueCoding {
+    extension: Vec<Extension>,
+    code: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Extension {
+    url: String,
+    value_meta: Option<ValueMeta>,
+    value_coding: Option<ValueCoding>,
+    value_string: Option<String>,
+    value_boolean: Option<bool>,
+    value_integer: Option<i64>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ValueMeta {
+    tag: Vec<Tag>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Tag {
+    #[serde(default)]
+    extension: Vec<Extension>,
+    code: String,
+    system: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Setting {
+    id: String,
+    label: String,
+    roles: Roles,
+    permissions_form_access: PermissionsFormAccess,
+    permissions_field_access: PermissionsFieldAccess,
+    elements: Vec<Element>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Roles {
+    id: String,
+    label: String,
+    #[serde(rename = "type")]
+    type_field: String,
+    items: Vec<Item>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Item {
+    key: String,
+    value: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PermissionsFormAccess {
+    id: String,
+    #[serde(rename = "type")]
+    type_field: String,
+    items: Vec<Item>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PermissionsFieldAccess {
+    id: String,
+    #[serde(rename = "type")]
+    type_field: String,
+    items: Vec<Item>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Element {
+    id: String,
+    title: String,
+    description: String,
+    section: Section,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Section {
+    id: String,
+    title: Option<String>,
+    description: Option<String>,
+    entries: Vec<Entry>,
+    signoff_ordering: Option<SignoffOrdering>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Entry {
+    role_permissions: Vec<RolePermission>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RolePermission {
+    role: Role,
+    permission: Option<Permission>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Role {
+    key: String,
+    value: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Permission {
+    key: String,
+    value: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct SignoffOrdering {
+    id: String,
+    hidden: bool,
+    label: String,
+    disabled: bool,
+    #[serde(rename = "type")]
+    type_field: String,
+    value: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct PdfTemplates {
+    id: String,
+    name: String,
+    description: String,
+    #[serde(rename = "type")]
+    type_field: String,
+    entries: Vec<Value>,
+    accept: Vec<String>,
+}
